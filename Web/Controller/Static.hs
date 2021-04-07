9c7d32f6c8d7b46f74b9c79c1fa7cfc03f12156d
Montmorency <lamberh@tcd.ie>
2021-04-07 16:22:42 +0100

    adding logging to test app.

--- Web/Controller/Static.hs
+++ Web/Controller/Static.hs
@@ -1,6 +1,9 @@
 module Web.Controller.Static where
 import Web.Controller.Prelude
 import Web.View.Static.Welcome
+import qualified IHP.Log as Log
 
 instance Controller StaticController where
-    action WelcomeAction = render WelcomeView
+    action WelcomeAction = do
+        Log.debug "Logging Operational."
+        render WelcomeView
