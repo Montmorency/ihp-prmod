50b2f6173e8c0610a28705bd888f8b2376e12d20
Montmorency <lamberh@tcd.ie>
2021-04-12 10:47:06 +0100

    Logging working fine using standard configuration.

--- Config/Config.hs
+++ Config/Config.hs
@@ -4,7 +4,16 @@ import IHP.Prelude
 import IHP.Environment
 import IHP.FrameworkConfig
 
+import qualified IHP.Log as Log
+import IHP.Log.Types
+
 config :: ConfigBuilder
 config = do
     option Development
     option (AppHostname "localhost")
+    logger <- liftIO $ newLogger def { 
+                              level = Debug,
+                              formatter = withTimeFormatter, 
+                              destination = File "Log/production.log" NoRotate defaultBufSize
+                            }
+    option logger
